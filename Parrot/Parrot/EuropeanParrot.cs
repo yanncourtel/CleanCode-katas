f0cdd05b43a7f2f41ab65be875f23665bcc5a2a5
Valentin Leon <valentin.leon@cdbdx.biz>
2021-07-20 10:38:32 +0200

    Refacto: EuropeanParrot

---
+++ Parrot/Parrot/EuropeanParrot.cs
@@ -0,0 +1,16 @@
+﻿namespace Parrot
+{
+    public class EuropeanParrot: BaseParrot
+    {
+        private readonly double BaseSpeed = 12.0;
+
+        public EuropeanParrot()
+        {
+        }
+
+        public override double GetSpeed()
+        {
+            return BaseSpeed;
+        }
+    }
+}
