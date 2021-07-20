6dff474daab51f01a1bb2495eca7b416289cf129
David Bourasseau <david.bourasseau@cdiscount.com>
2021-07-20 11:27:36 +0200

    liskov

--- SOLID/Liskov/GeometryTest.cs
+++ SOLID/Liskov/GeometryTest.cs
@@ -7,20 +7,17 @@ namespace SOLID.Liskov
         [Fact]
         public void Area_should_be_height_times_width_1()
         {
-            area_should_be_height_times_width(new Rectangle());
+            Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 4;
+            Assert.Equal(20, rect.Area);
         }
 
         [Fact]
         public void Area_should_be_height_times_width_2()
         {
-            area_should_be_height_times_width(new Square());
-        }
-
-        private void area_should_be_height_times_width(Rectangle rect)
-        {
-            rect.Width = 5;
-            rect.Height = 4;
-            Assert.Equal(20, rect.Area);
+            Square rect = new Square(5);
+            Assert.Equal(25, rect.Area);
         }
     }
 }
